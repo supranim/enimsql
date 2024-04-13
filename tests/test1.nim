f66d5c0759087ac6d11c6cadf1703d814a5bc5cf
George Lemon <georgelemon@protonmail.com>
2024-04-13 05:11:27 +0300

    rm default tests
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,48 +1,12 @@
+# This is just an example to get you started. You may wish to put all of your
+# tests into a single file, or separate them into multiple `test1`, `test2`
+# etc. files (better names are recommended, just make sure the name starts with
+# the letter 't').
+#
+# To run these tests, simply execute `nimble test`.
+
 import unittest
-import enimsql
-from std/strutils import escape
 
-model "User":
-    name: string
-    email: string
-    country: string
-    votes: int
-
-test "Meta: Table name pluralization":
-    assert User.getTableName() == "users"
-
-test "Meta: Object integrity -- Check for unknown column names":
-    try:
-        discard User.where(("email_address", EQ, "test@example.com")).getRaw()
-    except DatabaseDefect:
-        assert getCurrentExceptionMsg() == "Unknown column name \"email_address\" for model \"User\""
-
-# test "Meta: Object integrity -- Check duplicated column names":
-#     try:
-#         discard User.where(("email", EQ, "test@example.com"), ("email", EQ, "test@example.com")).getRaw()
-#     except DatabaseDefect:
-#         echo getCurrentExceptionMsg()
-
-test "WHERE Query 1":
-    let sql = User.where(("email", EQ, "test@example.com")).getRaw()
-    assert sql == "SELECT * FROM users WHERE email = 'test@example.com'"
-
-test "WHERE Query 2":
-    let sql = User.where(
-        ("email", EQ, "test@example.com"),
-        ("name", NEQ, "Lemon")
-    ).getRaw()
-    assert sql == "SELECT * FROM users WHERE email = 'test@example.com' AND name <> 'Lemon'"
-
-test "SELECT & WHERE Query 3":
-    let sql = User.select("name").where(("email", EQ, "test@example.com")).getRaw()
-    assert sql == "SELECT name FROM users WHERE email = 'test@example.com'"
-
-test "UPDATE Query 4":
-    let sql = User.update(("email", "new@example.com"))
-                  .where(("email", EQ, "test@example.com")).getRaw()
-    assert sql == "UPDATE users SET email = 'new@example.com' WHERE email = 'test@example.com'"
-
-test "UPDATE ALL Query 5":
-    let sql = User.updateAll(("votes", "1")).getRaw()
-    assert sql == "UPDATE users SET votes = '1'"
+import v2
+test "can add":
+  check add(5, 5) == 10
